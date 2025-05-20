82366fc88fcc78ced7b8377815b87ccb7b9b7f68
Lukec1103 <lukahrastnikluka@gmail.com>
2025-05-20 20:57:06 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Create from './pages/Create'
+import NotFound from './pages/NotFound'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/create">Create</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
